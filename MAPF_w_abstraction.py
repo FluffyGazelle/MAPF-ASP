b3408aaccae434b3a0e5baa71be49a478db5e4b9
Tunahan Yıldız <mtunahan@sabanciuniv.edu>
2024-05-13 00:21:46 +0300

    stopped using optimizer, fixing doors

--- MAPF_w_abstraction.py
+++ MAPF_w_abstraction.py
@@ -10,13 +10,13 @@ import os
 
 class Graphs():
 
-    def parse_output(self, sol, num_agents):
+    def parse_abstract_output(self, sol, num_agents):
         sol = sol.splitlines()[::-1]
         print(sol)
-        max_plan_length = total_plan_length = 0
+        
 
         if "UNSATISFIABLE" in sol:
-            return "UNSATISFIABLE", 0,0, []
+            return "UNSATISFIABLE", 0, 0, 0
 
         plans = dict()
         planLengths = dict()
@@ -27,14 +27,7 @@ class Graphs():
                 l = line.split()
                 cpu_time = l[-1]    
                 cpu_found = True
-
-            if "Optimization :" in line and not opt_found:
-                l = line.split()
-                # print(l)
-                max_plan_length = l[2]
-                total_plan_length = l[3]
-                opt_found = True
-
+                
             if "plan" in line and not plan_found:
                 # print(line)
                 solution = line.split()
@@ -77,12 +70,59 @@ class Graphs():
                 #print("plans", plans)
                 plan_found = True
             
-            if cpu_found and opt_found and plan_found:
+            if cpu_found and plan_found:
                 #print(plans)
-                return plans, planLengths, doors, [max_plan_length, total_plan_length, cpu_time]
+                print("plan lengths", planLengths)
+                return plans, planLengths, doors, cpu_time
+            
+    def parse_output(self, sol, num_agents):
+        sol = sol.splitlines()[::-1]
+        print(sol)
+        
+
+        if "UNSATISFIABLE" in sol:
+            return "UNSATISFIABLE", 0
+
+        plans = dict()
+        planLengths = dict()
+        doors = {}
+        cpu_found = opt_found = plan_found = False
+        for line in sol:
+            if "CPU" in line and not cpu_found:
+                l = line.split()
+                cpu_time = l[-1]    
+                cpu_found = True
+
+            if "plan" in line and not plan_found:
+                # print(line)
+                solution = line.split()
+                #solution = line.replace("plan(","").replace(")","").split()
+                #solution = [item.split(",") for item in solution]
+
+                for item in solution:
+                    if "plan(" in item:
+                        item = item.replace("plan(","").replace(")","")
+                        parts = item.split(",")
+                        agent = int(parts[0])
+                        time = int(parts[1])
+                        pos = int(parts[2])
+                        if agent not in plans:  # Initialize if not already present
+                            plans[agent] = {}
+                            doors[agent] = {}
+
+                        plans[agent][time] = pos
+
+                    
+                #print(planLengths)
+                #print("plans", plans)
+                plan_found = True
+            
+            if cpu_found and plan_found:
+                #print(plans)
+                return plans, cpu_time
     
     def run_clingo_for_abstract_problem():
-        cmd_list = ["clingo", "Abstract_Graph.lp", "Abstract_Agents.lp", "abstract_mapf.lp", "optimizations.lp"]
+        cmd_list = ["clingo", "Abstract_Graph.lp", "Abstract_Agents.lp", "abstract_mapf.lp"]
         print("Solving MAPF...")
         sol= sp.Popen(cmd_list, stdout=sp.PIPE, stderr = sp.PIPE, encoding = 'utf8').communicate()[0]
         return sol
@@ -90,11 +130,19 @@ class Graphs():
     def run_clingo_for_subproblem(subgraph_file,subproblem_agents_file):
         print(subgraph_file)
         print(subproblem_agents_file)
-        cmd_list = ["clingo", subgraph_file, subproblem_agents_file, "mapf.lp","subproblem_limit.lp","optimizations.lp"]
+        cmd_list = ["clingo", subgraph_file, subproblem_agents_file, "mapf.lp","subproblem_limit.lp"]
         print("Solving MAPF for subproblem...")
         sol= sp.Popen(cmd_list, stdout=sp.PIPE, stderr = sp.PIPE, encoding = 'utf8').communicate()[0]
         return sol
     
+    def get_max_manhattan_distance(G):
+        lowest = min(G.nodes(), key=lambda x: x[0] + x[1])
+        highest = max(G.nodes(), key=lambda x: x[0] + x[1])
+
+        # Calculate Manhattan distance
+        manhattan_distance = abs(lowest[0] - highest[0]) + abs(lowest[1] - highest[1])
+
+        return manhattan_distance
     """
     Problem 1: GRAPH PARTITIONING
     """
@@ -112,7 +160,7 @@ class Graphs():
         
         """
         color_map = [f'C{G.nodes[node]["cluster"]}' for node in G.nodes()]
-        plt.figure(figsize=(20, 20))
+        plt.figure(figsize=(15, 15))
         nx.draw(G, pos=positions, node_color=color_map, with_labels=True, node_size= 100, font_weight='bold')
         plt.title('Graph Partitioning')
         plt.show()
@@ -204,6 +252,7 @@ class Graphs():
             for H_j_prime in modified_subgraphs:
                 if H_i_prime.graph['label'] < H_j_prime.graph['label']:  
                     intersection = set(H_i_prime.nodes()) & set(H_j_prime.nodes())
+                    print(H_j_prime.graph['label'], H_i_prime.graph['label'],"intersection", intersection)
                     if intersection:
                         G_A.add_edge(H_i_prime.graph['label'], H_j_prime.graph['label'], weight=len(intersection), doors = intersection) 
                     edge_weights = nx.get_edge_attributes(G_A, 'weight')
@@ -243,10 +292,11 @@ class Graphs():
         # Write initial positions and goals
         for agent, (initial_pos, goals) in A_s.items():
             # Convert initial position
-            print("initial_pos", initial_pos)
+            print("initial_pos for agent ", agent, initial_pos)
+            print("initial and goal for agent ", agent, goals)
+            print(H_i_prime.nodes())
             key = next((k for k, v in value_to_coord.items() if v == initial_pos), None)
             output_lines.append(f"init({agent},{key}).")
-            print("GOALL",goals)
             
             # Convert each goal position
             for goal in goals:
@@ -294,6 +344,8 @@ class Graphs():
             
         abs_edges = G_A_with_colors.edges(data=True)
         edge_value_list = []
+        doors_formatted_string = ""
+        formatted_string = ""
         for node1, node2, attr in abs_edges:
             edge_str = str(node1)+", "+str(node2)
             reverse_edge_str = str(node2)+", "+str(node1)
@@ -302,20 +354,25 @@ class Graphs():
             edge_value_list.append(reverse_edge_str)
             
             doors = attr.get("doors")
+            print("I am eager to learn wtf this is",edge_str, doors)
             abstract_graph_doors_list = list()
             for door in doors:
                 key = next((k for k, v in value_to_coord.items() if v == door), None)
                 abstract_graph_doors_list.append(key)
-            doors_formatted_string = ""
-            for edge in edge_value_list:
-                input_str = f"door(edge({edge})"
-                print(input_str)
-                doors_formatted_string += ".".join([f"{input_str},{num})" for num in abstract_graph_doors_list]) + ".\n"
-                print("a", doors_formatted_string)
+            
+            
+            input_str = f"door(edge({edge_str})"
+            print("found it haha",input_str)
+            doors_formatted_string += ".".join([f"{input_str},{num})" for num in abstract_graph_doors_list]) + ".\n"
+            reverse_input_str = f"door(edge({reverse_edge_str})"
+            doors_formatted_string += ".".join([f"{reverse_input_str},{num})" for num in abstract_graph_doors_list]) + ".\n"
             print("b",doors_formatted_string)
-            formatted_string = ".".join([f"edge({num})" for num in edge_value_list])
+            
+            formatted_string += ".".join([f"edge({num})" for num in edge_value_list[-2:]]) + "."
+        
         with open(f"Abstract_Graph.lp", "a") as file:
-            file.write(doors_formatted_string + formatted_string+".\n")    
+            file.write(doors_formatted_string + formatted_string+"\n")
+                
         formatted_string = ".".join([f"node_cap({key}, {value})" for key, value in node_capacities.items()])
         with open("Abstract_Graph.lp", "a") as file:
             file.write(formatted_string+".\n")
@@ -348,52 +405,56 @@ if __name__ == "__main__":
     sys.stdout = logFile
     sys.stderr = logFile
     #READ AGENTS FROM FILE
-    with open("16x16_agents/5_agents/5_agents_1.txt", "r") as file:
+    print("Hi, starting the program...")
+    
+    n = 2 # Number of clusters
+    grid_size = (4, 8) 
+    makespan_limit = n
+    G = nx.grid_2d_graph(*grid_size)
+
+    with open(f"{grid_size[0]}x{grid_size[1]}_agents/5_agents_1.txt", "r") as file:
         agents = file.read()
         agents_list = agents.split("\n")
-        
         
     Agents = []
     for agent in agents_list:
         numbers_list = list(map(int, agent.split()))  # Convert each number to an integertegerr   
         paired_numbers = ((numbers_list[0], numbers_list[1]), (numbers_list[2], numbers_list[3]))
-        Agents.append(paired_numbers)
+        Agents.append(paired_numbers) 
+    print("agents:",Agents)       
+    print("Generated the grid graph")
     
-    
-    print("agents:",Agents)
-    n = 2 # Number of clusters
-    grid_size = (4, 8) 
-    makespan_limit = 5
-    G = nx.grid_2d_graph(*grid_size)
+      
 
-    
     positions = {node: (node[0], node[1]) for node in G.nodes()}
 
     value_to_coord = {(initx * max(grid_size)) + inity: (initx, inity) for initx, inity in G.nodes()}
     coord_to_value = {coord: value for value, coord in value_to_coord.items()}
 
+    print("value to coord", coord_to_value)
     
     subgraphs = Graphs.graph_partitioning(G, n)
 
     modified_subgraphs = Graphs().subgraph_intersecting(G, subgraphs)
     subgraph_colors = [f'C{i}' for i in range(len(modified_subgraphs))]
 
-    """
     
+    """
     for i, subgraph in enumerate(subgraphs):
         plt.figure(figsize=(5, 5))
         nx.draw(subgraph, pos=positions, with_labels=True, node_color=f'C{i}', font_weight='bold')
         plt.title(f'original Subgraph {i+1}')
     
     plt.show()
+    
 
     for i, subgraph in enumerate(modified_subgraphs):
-        plt.figure(figsize=(10, 10))
+        plt.figure(figsize=(5, 5))
         nx.draw(subgraph, pos=positions, with_labels=True, node_color=f'C{i}', font_weight='bold')
         plt.title(f'Modified Subgraph {i+1}')
 
     plt.show()
-    """
+"""
     
     # Generate the abstract graph with color
     G_A_with_colors, node_capacities , edge_capacities = Graphs.abstract_graph_representation(modified_subgraphs, subgraph_colors)
@@ -403,7 +464,7 @@ if __name__ == "__main__":
     pos=nx.circular_layout(G_A_with_colors)
     pos=nx.spring_layout(G_A_with_colors,dim=2,pos=pos)
 
-    """
+    
     plt.figure(figsize=(10, 10))
     nx.draw(G_A_with_colors,
             with_labels=True,
@@ -413,28 +474,31 @@ if __name__ == "__main__":
             font_weight='bold')
     plt.title("Abstract Graph Representation with Subgraph Colors")
     plt.show()
+
     
-    """
     
     # Generating the mapf instance files
     Graphs().generate_lp_files(modified_subgraphs, G_A_with_colors, node_capacities, edge_capacities, A_A, value_to_coord, makespan_limit)
 
+    print("Running MAPF solver...")
     sol = Graphs.run_clingo_for_abstract_problem()
-    print(sol)
-    solution, plan_lengths, doors_for_plans, opts = Graphs().parse_output(sol, len(A_A))
+    print("sol",sol)
+    solution, plan_lengths, doors_for_plans, opts = Graphs().parse_abstract_output(sol, len(A_A))
     print("doors",doors_for_plans)
     print(plan_lengths)
     print("solution",solution)
     if solution == "UNSATISFIABLE":
         print("No solution found for the given instance!")
         sys.exit(0)
-        
+
     plans = {}
     
     longest_abstract_plan = max(plan_lengths.values())
     Agents_dict = {agent: Agents[agent] for agent in range(len(Agents))}
     PLANS = {}
     last_positions = {agent: Agents[agent][0] for agent in range(len(Agents))}
+    
+
     for abstract_step in range(longest_abstract_plan+1):
         print("\n\n\n\n\n\nabstract_step", abstract_step)
         print("Agents_dict", Agents_dict)
@@ -448,7 +512,6 @@ if __name__ == "__main__":
             A_s = {}
             for agent_key in Agents_dict.keys():
                 agent_value = Agents_dict[agent_key]
-                print("agent", agent_key, agent_value)
                 print(solution)
                 print("agent_solution", solution[agent_key], abstract_step)
                 
@@ -476,11 +539,7 @@ if __name__ == "__main__":
                             
                             filtered_nodes = [coord_to_value.get(node) for node in H_i.nodes() if coord_to_value.get(node) not in cannot_be_goal_nodes]
                             print("filtered nodes",filtered_nodes)
-                            subproblem_goal_pos = subproblem_goal_pos + filtered_nodes
-                            
-                    
-                    print("subproblem goal positions",subproblem_goal_pos)
-                        
+                            subproblem_goal_pos = subproblem_goal_pos + filtered_nodes                        
 
                         
                     init_goal_pos_for_agent_for_subproblem = (subproblem_initial_pos,subproblem_goal_pos)
@@ -490,19 +549,23 @@ if __name__ == "__main__":
             print("A_s", A_s)
             if not A_s == {}:
                 A_s_STR =Graphs.subproblem_agents_to_str(A_s, grid_size)
-                print("A_s_STR", A_s_STR)
                 output_path = f"subproblem_agents_for_step_{abstract_step}_subgraph_{H_i_prime.graph['label']}.lp"
                 with open(output_path, 'w') as file:
                     file.write(A_s_STR)
-                
-                s_sol = Graphs.run_clingo_for_subproblem(f"subgraph_{H_i_prime.graph['label']}.lp", f"subproblem_agents_for_step_{abstract_step}_subgraph_{H_i_prime.graph['label']}.lp")
-                
-                print(s_sol)
-                s_solution, s_plan_lengths, nvm, s_opts = Graphs().parse_output(s_sol, len(A_A))
-                print(s_solution)
-                if s_solution == "UNSATISFIABLE":
-                    print("No solution found for the given instance!")
-                    sys.exit(0)
+                max_manhattan_distance = Graphs.get_max_manhattan_distance(H_i_prime)
+                with open("subproblem_limit.lp", "w") as file:
+                    file.write(f"#const t = {max_manhattan_distance}.\ntime(0..t).")
+                while True:
+                    s_sol = Graphs.run_clingo_for_subproblem(f"subgraph_{H_i_prime.graph['label']}.lp", f"subproblem_agents_for_step_{abstract_step}_subgraph_{H_i_prime.graph['label']}.lp")
+                    
+                    print(s_sol)
+                    s_solution, cpu_time_for_subproblem = Graphs().parse_output(s_sol, len(A_A))
+                    print(s_solution)
+                    if s_solution == "UNSATISFIABLE":
+                        print("No solution found for the given instance!")
+                        max_manhattan_distance += 1
+                    else:
+                        break
                 print("slution:", s_solution)
                 last_elements = {key: value_to_coord[values[max(values.keys())]] for key, values in s_solution.items()}
                 
